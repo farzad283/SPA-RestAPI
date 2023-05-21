ea9cfc94de87a94801280b63ce1ef3e1fb3ca5aa
Farzad <farzad@Farzads-MacBook-Air.local>
2023-05-20 23:29:19 -0400

    modifier css

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -10,8 +10,8 @@ export default class extends AbstractView{
         return `
         <div class=position_Dash>
         <h1><a href="/foods" data-link>Rechercher des recettes</a></h1>
-        <p>Recherchez parmi des milliers de recettes à l'aide d'un filtrage et d'un classement avancés. REMARQUE : Cette méthode combine la recherche par requête, par ingrédients et par nutriments en un seul point final.</p>
         <img class=position_img src="static/img/recherch.jpg">
+        <p>Recherchez parmi des milliers de recettes à l'aide d'un filtrage et d'un classement avancés. REMARQUE : Cette méthode combine la recherche par requête, par ingrédients et par nutriments en un seul point final.</p>
         </div>
         `
 
